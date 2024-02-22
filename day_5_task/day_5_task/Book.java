1712fd5b5951bbe1316f48e6b59cba7de369246f
Priyadharsini1003 <126433343+Priyadharsini1003@users.noreply.github.com>
2024-02-22 23:03:48 +0530

    Add files via upload

---
+++ day_5_task/day_5_task/Book.java
@@ -0,0 +1,38 @@
+package guvitask;
+
+
+class Book {
+    private int bookID;
+    private String title;
+    private String author;
+    private boolean isAvailable;
+
+    public Book(int bookID, String title, String author, boolean isAvailable) {
+        this.bookID = bookID;
+        this.title = title;
+        this.author = author;
+        this.isAvailable = isAvailable;
+    }
+
+    // Getters and setters
+    public int getBookID() {
+        return bookID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+}
+
---
+++ day_5_task/day_5_task/Taxable.java
@@ -0,0 +1,9 @@
+package product;
+
+public interface Taxable {
+    double SALES_TAX = 0.07;
+    double INCOME_TAX = 0.105;
+
+    double calcTax();
+}
+
